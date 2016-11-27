e08cd4cf776d599c68a67218c6094d282315da8e
Alexhodge <willamhodge@yahoo.com>
2016-11-27 00:03:15 -0600

    This is a great place to start building the app
    At this point the app has the bare basic functionality we are looking for. This is a good foundation. From here we can expand the app.
    I have added a feature so that a team can be signed up when they are trying to sign in(in case they failed to sign up in the first place).

--- TeamCheckIn.rb
+++ TeamCheckIn.rb
@@ -1,34 +1,38 @@
-teamArray = []
-teamNameArray = []
-teamCheckArray = []
-cnt = 0
+teamNameArray = [] # this is the array that is filled when teams SIGN UP for the tournament
+teamCheckArray = [] # this is the array that is filled when team SIGN IN for the tournament
 quitcheck1 = 0
-quitcheck2 = 0
 
 
-while (quitcheck1 != 1)
+while (quitcheck1 != 1) #Gathering team names which will be stored in a database i assume. Dont know how to do that tho.
     puts "Please enter your team name"
-    tempname = gets.chomp
+    tempname = gets.chomp.downcase
     teamNameArray << tempname
-    puts "If all names have been entered press 1, otherwise press 0"
+
+    puts "If all names have been entered press 1, otherwise press 0" # We can get rid of "pres 1 or 0"but i think it helps to keep the program simple and regimented right now.
     quitcheck1 = gets.to_i
-
 end
 
-while (quitcheck2 != 1)
-  puts "Please enter your team name to sign in"
-  tempname2 = gets.chomp
-  teamCheckArray << tempname2
-  puts "If you would like to check and see what teams have been checked in press 1, otherwise press 0."
-  quitcheck2 = gets.to_i
 
-end
+  while (teamNameArray != [])
+    puts "Please enter your team name to sign in"   #This else loop tells if a team is not signed up and allowes them to sign up and be added to the teamNameArray so that they can sign in
+    tempname2 = gets.chomp.downcase
+    teamCheckArray << tempname2
+    quitcheck2 = gets.to_i
 
-while(teamCheckArray != [""])
-  if teamCheckArray.include?(tempname2)
-    teamCheckArray.delete(tempname2)
-    
-  else
-    puts "Your team was not found in our system!"
-end
+      if teamNameArray.include?(tempname2)
+        teamNameArray.delete(tempname2)
+
+      else
+        teamCheckArray.delete(tempname2)
+        puts "Your team was not found in our system!"
+        puts "Please sign up now by entering your team name"
+        tempname = gets.chomp.downcase
+        teamNameArray <<tempname
+
+  end
+
+
+  puts "Teams that have signed up but have not signed in are:  #{teamNameArray}."
+  puts "Teams that have signed up and that have signed in are: #{teamCheckArray}."
+
 end
